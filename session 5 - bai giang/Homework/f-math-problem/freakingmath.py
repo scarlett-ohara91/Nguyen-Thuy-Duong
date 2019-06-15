b7393842ac94af3109a9bfc15c2a4d636e856466
scarlett-ohara91 <thuyduong91.finance@gmail.com>
2019-06-15 11:56:52 +0700

    '.'

--- session 5 - bai giang/Homework/f-math-problem/freakingmath.py
+++ session 5 - bai giang/Homework/f-math-problem/freakingmath.py
@@ -1,29 +1,38 @@
 from random import *
 
-def eval(x,y,ope):
-    if ope == '+':
+def eval(x,y,oper):
+    if oper == '+':
         return x + y
-    if ope == '-':
+    if oper == '-':
         return x - y
-    if ope == '*':
+    if oper == '*':
         return x * y
-    if ope == '/':
+    if oper == '/':
         return x / y
 
 
 def generate_quiz():
     from random import randint
 
-    x = randint(0,10)
-    y = randint(0,10)
+    x = randint(1,10)
+    y = randint(1,10)
     list_oper = ['+','-','*','/']
     oper = list_oper[randint(0,3)]
 
-    result = eval(x,y,oper)
+    result_raw = eval(x,y,oper)
     random_result = randint(-1,1)
-    present_result = random_result + result
+    result = random_result + result_raw
 
-    return [x, y, oper, present_result]
+    return [x, y, oper, result]
 
 def check_answer(x, y, op, result, user_choice):
-    pass
+    if eval(x,y,op) == result:
+        if user_choice == True:
+            return True
+        else:
+            return False
+    elif eval(x,y,op) != result:
+        if user_choice == True:
+            return False
+        else:
+            return True
